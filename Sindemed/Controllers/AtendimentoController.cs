880676368d16a25b3e061dd5e953f528d3be1c41
André Borges Leal <andreborgesleal@live.com>
2013-12-29 22:57:56 -0300

    arquivos que nao subiram no commit anterior

---
+++ Sindemed/Controllers/AtendimentoController.cs
@@ -0,0 +1,34 @@
+﻿using App_Dominio.Controllers;
+using App_Dominio.Security;
+using Sindemed.Models.Persistence;
+using Sindemed.Models.Repositories;
+using System;
+using System.Web.Mvc;
+
+namespace Sindemed.Controllers
+{
+    public class AtendimentoController : ProcessController<AtendimentoViewModel, AtendimentoModel>
+    {
+        public override int _sistema_id() { return (int)Sindemed.Models.Enumeracoes.Sistema.SINDMED; }
+        public override string getListName()
+        {
+            return "Listagem de Atendimentos";
+        }
+
+        #region List
+        [AuthorizeFilter]
+        public override ActionResult List(int? index, int? pageSize = 50, string descricao = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        [AuthorizeFilter]
+        public ActionResult List(int? index, int? pageSize = 50, int? chamadoId = null,
+                                    int? associadoId = null, string data1 = null, string data2 = null, int? areaAtendimento = null, string situacao = null)
+        {
+            ListViewChamadoAdministracao l = new ListViewChamadoAdministracao();
+            return _List(index, pageSize, "Browse", l, chamadoId, associadoId, DateTime.Parse(data1), DateTime.Parse(data2), areaAtendimento, situacao);
+        }
+        #endregion
+	}
+}
