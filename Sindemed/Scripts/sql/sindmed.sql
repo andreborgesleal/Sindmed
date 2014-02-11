f389ca5cd47aec10b0c6430f92ee742827d1e11d
André Borges Leal <andreborgesleal@live.com>
2014-02-10 23:53:36 -0300

    Ajustes nos links para download, inclusão de formulário modal para exibição do conteúdo html ou imagem

--- Sindemed/Scripts/sql/sindmed.sql
+++ Sindemed/Scripts/sql/sindmed.sql
@@ -2,3 +2,5 @@
 
 truncate table AssociadoDocumento
 select * from AssociadoDocumento
+
+update AssociadoDocumento set fileId = 'e1756448-0c22-436c-adc2-835171cff317.pdf' where nomeArquivoOriginal = 'Segundo teste'
